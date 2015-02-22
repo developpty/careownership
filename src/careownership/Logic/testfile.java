3d22a3c9c436196a98cd9e480334901923126006
Dalun Bian <biandalun@gmail.com>
2015-02-22 08:38:28 -0500

    Update and rename src/careownership/Logic to src/careownership/Logic/testfile.java

---
+++ src/careownership/Logic/testfile.java
@@ -0,0 +1,13 @@
+package careownership.Logic;
+
+import java.io.IOException;
+
+public class testfile {
+
+	public static void main(String[] args) throws IOException {
+		// TODO Auto-generated method stub
+		ReportLogic a = new ReportLogic();
+		a.genCSVfile(2);
+	}
+
+}
